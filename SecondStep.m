587ae0f812e526facaaff79c1570a7a7623605d2
Maulin Amin <Maulin Amin@MaulinAmin-Dell>
2018-03-02 00:20:31 -0500

    Creates a table of low and high values from each station

--- SecondStep.m
+++ SecondStep.m
@@ -80,3 +80,17 @@ MaxPoints{8,1} = CollectMaxDataPointsForAStation(folder8);
 %Sort and rewrite the maximum data points for Toronto
 temp = (MaxPoints{8,1});
 MaxPoints{8,1} = sortrows(temp,2,'descend');
+
+for k = 1:8
+    T = MaxPoints{k,1};
+    H = height(T);
+    R(k,1) = T(H,1);
+    R(k,2)= T(H,2);
+    R(k,3) = T(1,1);
+    R(k,4) = T(1,2);
+end
+City_Names = {'Toronto Airport','Trenton','Toronto Island','London','Wiarton','KW','Hamilton','Sarnia'};
+R.Properties.VariableNames = {'Min_Date_Time','Min_SpdOfMaxGust_km_h_','Max_Date_Time','Max_SpdOfMaxGust_km_h_'}
+R.Properties.RowNames = City_Names;
+% % MaxPoints{1,1}(H,:)
+% % Range{1,2} = MaxPoints{1,1}(1,:)
